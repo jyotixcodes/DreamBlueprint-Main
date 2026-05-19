71ede01c98c260adcdb5380e9dc68f2b0218dbc1
Jyoti <csepampa@gmail.com>
2026-05-19 10:50:06 +0530

    Add backend source code

---
+++ dream-blueprint-backend/src/main/java/com/dreamblueprint/backend/DreamBlueprintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dreamblueprint.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamBlueprintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamBlueprintBackendApplication.class, args);
+	}
+
+}
---
+++ dream-blueprint-backend/src/test/java/com/dreamblueprint/backend/DreamBlueprintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamblueprint.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamBlueprintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
